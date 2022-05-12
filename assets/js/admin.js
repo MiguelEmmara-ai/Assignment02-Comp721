e8c70cbd80838718f460e6c630aca7b74361260e
Miguel Emmara <m.miguelemmara@gmail.com>
2022-05-13 11:47:06 +1200

    add more btn on admin.html unsuccessful attempt on displaying current date and time booking.html
    add js function showRecent*
    
    more todo

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,34 @@
 /**
+ * This function shows recent bookings (within 2 hours) by passing information to the server
+ * @send XML object
+ */
+function showRecentHtml() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("tableID").innerHTML = this.responseText;
+        }
+    }
+    xmlhttp.open("GET", 'includes/backend/getRecentBookHtml.php', true);
+    xmlhttp.send();
+}
+
+/**
+ * This function shows recent bookings (within 2 hours) by passing information to the server
+ * @send XML object
+ */
+function showRecent() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("tableID").innerHTML = this.responseText;
+        }
+    }
+    xmlhttp.open("GET", 'includes/backend/getRecentBook.php', true);
+    xmlhttp.send();
+}
+
+/**
  * This function shows all bookings by passing information to the server
  * @send XML object
  */
@@ -6,7 +36,7 @@ function showall() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("showAlliD").innerHTML = this.responseText;
+            document.getElementById("tableID").innerHTML = this.responseText;
         }
     }
     xmlhttp.open("GET", 'includes/backend/getAllBook.php', true);
@@ -21,7 +51,7 @@ function shoAvailPassengers() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("showAlliD").innerHTML = this.responseText;
+            document.getElementById("tableID").innerHTML = this.responseText;
         }
     }
     xmlhttp.open("GET", 'includes/backend/getAvailBook.php', true);
@@ -36,7 +66,7 @@ function showallHtml() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("showAlliD").innerHTML = this.responseText;
+            document.getElementById("tableID").innerHTML = this.responseText;
         }
     }
     xmlhttp.open("GET", 'includes/backend/getAllBookHtml.php', true);
@@ -51,7 +81,7 @@ function shoAvailPassengersHtml() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("showAlliD").innerHTML = this.responseText;
+            document.getElementById("tableID").innerHTML = this.responseText;
         }
     }
     xmlhttp.open("GET", 'includes/backend/getAvailBookHtml.php', true);
@@ -66,7 +96,7 @@ function updateAssignCab(bookingRefNo) {
     var xhttp = createRequest();
 
     if (bookingRefNo == "") {
-        document.getElementById("showAlliD").innerHTML = "";
+        document.getElementById("tableID").innerHTML = "";
         return;
     }
 
@@ -92,7 +122,16 @@ function updateAssignCabHtml(bookingRefNo) {
     var xhttp = createRequest();
 
     if (bookingRefNo == "") {
-        document.getElementById("showAlliD").innerHTML = "";
+        $(document).ready(function() {
+
+            swal({
+                html: true,
+                title: "Oh No...",
+                text: "Please Fill The Booking Reference",
+                icon: "error",
+                button: "OK",
+            })
+        });
         return;
     }
 
---
+++ includes/backend/getRecentBookHtml.php
@@ -0,0 +1,67 @@
+<?php
+define('MY_CONSTANT', 1);
+
+// Include config file
+include(dirname(__FILE__) . "/../dbconf/settings.php");
+
+mysqli_select_db($conn, $dbnm);
+// TODO fix create new colum for date and time
+$query = "SELECT * FROM passengers WHERE status = 'Unassigned', pickUpTime > now() - interval 2 hour;";
+$result = mysqli_query($conn, $query);
+?>
+
+<table class="table table-striped table tablesorter" id="ipi-table">
+    <thead class="thead-dark">
+        <tr>
+            <th class="text-center">Booking ref no</th>
+            <th class="text-center">customer name</th>
+            <th class="text-center">phone number</th>
+            <th class="text-center filter-false sorter-false">unit number</th>
+            <th class="text-center filter-false sorter-false">street number</th>
+            <th class="text-center filter-false sorter-false">street name</th>
+            <th class="text-center filter-false sorter-false">suburb</th>
+            <th class="text-center filter-false sorter-false">destination suburb</th>
+            <th class="text-center filter-false sorter-false">pickup date</th>
+            <th class="text-center filter-false sorter-false">pickup time</th>
+            <th class="text-center filter-false sorter-false">status</th>
+            <th class="text-center filter-false sorter-false">Cars Need</th>
+            <th class="text-center filter-false sorter-false">Assigned By</th>
+            <th class="text-center filter-false sorter-false">actions</th>
+        </tr>
+    </thead>
+
+    <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+
+        <tbody class="text-center">
+            <tr id="<?= $row["bookingRefNo"] ?>">
+                <td><?= $row["bookingRefNo"] ?></td>
+                <td><?= $row["customerName"] ?></td>
+                <td><?= $row["phoneNumber"] ?></td>
+                <td><?= $row["unitNumber"] ?></td>
+                <td><?= $row["streetNumber"] ?></td>
+                <td><?= $row["streetName"] ?></td>
+                <td><?= $row["suburb"] ?></td>
+                <td><?= $row["destinationSuburb"] ?></td>
+                <td><?= $row["pickUpDate"] ?></td>
+                <td><?= $row["pickUpTime"] ?></td>
+                <td><?= $row["status"] ?></td>
+                <td><img src="assets\img\cars\<?= $row["carsNeed"] ?>.png" alt="<?= $row["carsNeed"] ?>"><br><?= $row["carsNeed"] ?></td>
+                <td><?= $row["assignedBy"] ?></td>
+
+                <?php if ($row['status'] == "Assigned") : ?>
+                    <td class="text-center align-middle" style="max-height: 60px;height: 60px;"><a class="btn btn-primary disabled" role="button" aria-disabled="true"><i class="far fa-paper-plane"></i>&nbsp;ASSIGN</a></td>
+                <?php else : ?>
+                    <td class="text-center align-middle" style="max-height: 60px;height: 60px;"><a class="btn btn-primary" role="button" onClick="updateAssignCabHtml('<?= $row["bookingRefNo"] ?>')"><i class="far fa-paper-plane"></i>&nbsp;ASSIGN</a></td>
+                <?php endif; ?>
+
+            </tr>
+        </tbody>
+
+    <?php } ?>
+
+    <?php
+    // Frees up the memory, after using the result pointer
+    mysqli_free_result($result);
+
+    // Close Connection
+    mysqli_close($conn); ?>
