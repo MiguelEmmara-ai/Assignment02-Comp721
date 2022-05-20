cb430b8074a4c76e7ce6c20bf39b85c37fc5eef3
Miguel Emmara <m.miguelemmara@gmail.com>
2022-05-20 23:09:55 +1200

    Add some more comments for includes\dbconf

--- includes/dbconf/settings.php
+++ includes/dbconf/settings.php
@@ -1,14 +1,23 @@
 <?php
-$host="localhost";
-$user="root";
-$pswd="";
-$dbnm="cabsonline";
+$host = "localhost";
+$user = "root";
+$pswd = "";
+$dbnm = "cabsonline";
 
-// Create connection
+/*
+|--------------------------------------------------------------------------
+| Create Connection
+|--------------------------------------------------------------------------
+|
+| Here are each of the database connections 
+| setup for your application.
+| simple mysqli connection setup make development simple.
+|
+ */
+
 $conn = new mysqli($host, $user, $pswd, $dbnm);
 
-// Check connection
+// Check connection 
 if ($conn->connect_error) {
     die("Connection Failed: " . $conn->connect_error);
 }
-?>
